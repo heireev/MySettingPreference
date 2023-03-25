c8dd5840ba044c3e8e1d89cf0a82db774e66c11c
heireev <muhammadrifqi216@gmail.com>
2023-03-26 02:20:57 +0800

    [fixed] Masalah ChechBox

--- app/src/main/java/com/reev/mysettingpreference/MyPreferenceFragment.kt
+++ app/src/main/java/com/reev/mysettingpreference/MyPreferenceFragment.kt
@@ -6,9 +6,7 @@ import androidx.preference.CheckBoxPreference
 import androidx.preference.EditTextPreference
 import androidx.preference.PreferenceFragmentCompat
 
-class MyPreferenceFragment : PreferenceFragmentCompat(),
-    SharedPreferences.OnSharedPreferenceChangeListener {
-
+class MyPreferenceFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedPreferenceChangeListener {
     private lateinit var NAME: String
     private lateinit var EMAIL: String
     private lateinit var AGE: String
@@ -19,10 +17,10 @@ class MyPreferenceFragment : PreferenceFragmentCompat(),
     private lateinit var emailPreference: EditTextPreference
     private lateinit var agePreference: EditTextPreference
     private lateinit var phonePreference: EditTextPreference
-    private lateinit var isLoveMuPreference: EditTextPreference
+    private lateinit var isLoveMuPreference: CheckBoxPreference
 
     companion object {
-        private const val DEFAULT_VALUE = "tidak_ada"
+        private const val DEFAULT_VALUE = "Tidak Ada"
     }
 
     override fun onCreatePreferences(bundle: Bundle?, s: String?) {
@@ -38,21 +36,11 @@ class MyPreferenceFragment : PreferenceFragmentCompat(),
         PHONE = resources.getString(R.string.key_phone)
         LOVE = resources.getString(R.string.key_love)
 
-        namePreference = findPreference<EditTextPreference>(NAME) as EditTextPreference
+        namePreference = findPreference<EditTextPreference> (NAME) as EditTextPreference
         emailPreference = findPreference<EditTextPreference>(EMAIL) as EditTextPreference
         agePreference = findPreference<EditTextPreference>(AGE) as EditTextPreference
         phonePreference = findPreference<EditTextPreference>(PHONE) as EditTextPreference
         isLoveMuPreference = findPreference<CheckBoxPreference>(LOVE) as CheckBoxPreference
-
-    }
-
-    private fun setSummaries() {
-        val sh = preferenceManager.sharedPreferences
-        namePreference.summary = sh.getString(NAME, DEFAULT_VALUE)
-        emailPreference.summary = sh.getString(EMAIL, DEFAULT_VALUE)
-        agePreference.summary = sh.getString(AGE, DEFAULT_VALUE)
-        phonePreference.summary = sh.getString(PHONE, DEFAULT_VALUE)
-        isLoveMuPreference.isChecked = sh.getBoolean(LOVE, false)
     }
 
     override fun onResume() {
@@ -65,21 +53,34 @@ class MyPreferenceFragment : PreferenceFragmentCompat(),
         preferenceScreen.sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
     }
 
-    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String) {
         if (key == NAME) {
-            namePreference.summary = sharedPreferences!!.getString(NAME, DEFAULT_VALUE)
+            namePreference.summary = sharedPreferences.getString(NAME, DEFAULT_VALUE)
         }
+
         if (key == EMAIL) {
-            emailPreference.summary = sharedPreferences!!.getString(EMAIL, DEFAULT_VALUE)
+            emailPreference.summary = sharedPreferences.getString(EMAIL, DEFAULT_VALUE)
         }
+
         if (key == AGE) {
-            agePreference.summary = sharedPreferences!!.getString(AGE, DEFAULT_VALUE)
+            agePreference.summary = sharedPreferences.getString(AGE, DEFAULT_VALUE)
         }
+
         if (key == PHONE) {
-            phonePreference.summary = sharedPreferences!!.getString(PHONE, DEFAULT_VALUE)
+            phonePreference.summary = sharedPreferences.getString(PHONE, DEFAULT_VALUE)
         }
+
         if (key == LOVE) {
-            isLoveMuPreference.isChecked = sharedPreferences?.getBoolean(LOVE, false)
+            isLoveMuPreference.isChecked = sharedPreferences.getBoolean(LOVE, false)
         }
+    }
+
+    private fun setSummaries() {
+        val sh = preferenceManager.sharedPreferences
+        namePreference.summary = sh.getString(NAME, DEFAULT_VALUE)
+        emailPreference.summary = sh.getString(EMAIL, DEFAULT_VALUE)
+        agePreference.summary = sh.getString(AGE, DEFAULT_VALUE)
+        phonePreference.summary = sh.getString(PHONE, DEFAULT_VALUE)
+        isLoveMuPreference.isChecked = sh.getBoolean(LOVE, false)
     }
 }
